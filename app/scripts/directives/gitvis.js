e955ab52af57b7dac597649b638cbe3628b05ec6
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-23 16:29:02 -0700

    push working d3 example

--- app/scripts/directives/gitvis.js
+++ app/scripts/directives/gitvis.js
@@ -3,7 +3,7 @@
 angular.module('githubleagueClientApp')
   .directive('gitvis', function () {
     return {
-      template: '<div class="chart"></div>',
+      template: '<div class="chart"><button>run</button></div>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         // element.text('this is the gitvis directive');
@@ -16,7 +16,7 @@ angular.module('githubleagueClientApp')
             x = d3.scale.ordinal().domain([57, 32, 112]).rangePoints([0, w], 1),
             y = d3.scale.ordinal().domain(data).rangePoints([0, h], 2);
 
-        var svg = d3.select(element).append('svg')
+        var svg = d3.select(element[0].firstChild).append('svg')
             .attr('height', h)
             .attr('width', w);
 
@@ -28,6 +28,25 @@ angular.module('githubleagueClientApp')
             .attr('cy', y)
             .attr('r', 20 );
 
+        d3.select('.chart button').on('click', function() {
+          svg.selectAll('.select').remove();
+
+          svg.selectAll('.select')
+              .data(data)
+            .enter().append('circle')
+              .attr('class', 'select')
+              .attr('cx', x)
+              .attr('cy', y)
+              .attr('r', 55)
+              .style('fill', 'none')
+              .style('stroke', 'red')
+              .style('stroke-width', 6)
+              .style('stroke-opacity', 8e-6)
+            .transition()
+              .duration(815)
+              .attr('r', 20)
+              .style('stroke-opacity', 1);
+        });
       }
     };
   });
