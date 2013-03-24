8a1023c112b5774ba553481e61bf47d218af2e4d
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-24 11:31:48 -0700

    add svg geo-json world map to main page

--- app/scripts/directives/gitvis.js
+++ app/scripts/directives/gitvis.js
@@ -3,51 +3,41 @@
 angular.module('githubleagueClientApp')
   .directive('gitvis', function () {
     return {
-      template: '<div class="chart"><button>run</button></div>',
+      template: "<div id='mapContainer'></div>",
       restrict: 'E',
       val: '=',
       link: function postLink(scope, element, attrs) {
         // element.text('this is the gitvis directive');
-        d3.tsv('/commitByTimeandLocation.csv', function (data) {
-          // console.log(data);
-          var data = scope.data,
-              w = 360,
-              h = 180,
-              x = d3.scale.ordinal().domain(data).rangePoints([0, w], 1),
-              y = d3.scale.ordinal().domain(data).rangePoints([0, h], 2);
+        function draw(ht) {
+          $("#mapContainer").html("<svg id='map' xmlns='http://www.w3.org/2000/svg' width='100%' height='" + ht + "'></svg>");
+          var map = d3.select("svg");
+          var width = $("svg").parent().width();
+          var height = ht;
 
-          var svg = d3.select(element[0].firstChild).append('svg')
-              .attr('height', h)
-              .attr('width', w);
+          // I discovered that the unscaled equirectangular map is 640x360. Thus, we
+          // should scale our map accordingly. Depending on the width ratio of the new
+          // container, the scale will be this ratio * 100. You could also use the height
+          // instead. The aspect ratio of an equirectangular map is 2:1, so that's why
+          // our height is half of our width.
 
-          svg.selectAll('.little')
-              .data(data)
-              .enter().append('circle')
-              .attr('class', 'little')
-              .attr('cx', x)
-              .attr('cy', y)
-              .attr('r', 20 );
-
-          d3.select('.chart button').on('click', function() {
-            svg.selectAll('.select').remove();
-
-            svg.selectAll('.select')
-                .data(data)
-              .enter().append('circle')
-                .attr('class', 'select')
-                .attr('cx', x)
-                .attr('cy', y)
-                .attr('r', 55)
-                .style('fill', 'none')
-                .style('stroke', 'red')
-                .style('stroke-width', 6)
-                .style('stroke-opacity', 8e-6)
-              .transition()
-                .duration(815)
-                .attr('r', 20)
-                .style('stroke-opacity', 1);
+          var projection = d3.geo.equirectangular().scale((width/640)*600).translate([width/2, height/2]);
+          var path = d3.geo.path().projection(projection);
+          d3.json('../geodata/world-map.json', function(collection) {
+              map.selectAll('path').data(collection.features).enter()
+                  .append('path')
+                  .attr('d', path)
+                  .attr("width", width)
+                  .attr("height", width/2);
           });
-        });
+        }
+        draw($("#mapContainer").width()/2);
+
+
+
+
+
+
+
       }
     };
   });
